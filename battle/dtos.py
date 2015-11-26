4d74732725663a667bdf23fb246a9092668cb045
CNicolas <clement92nicolas@gmail.com>
2015-11-26 09:31:23 +0100

    little corrections

--- battle/dtos.py
+++ battle/dtos.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 # @Author: cnicolas
 # @Date:   2015-11-19 16:37:40
-# @Last Modified by:   Aku
-# @Last Modified time: 2015-11-20 14:47:13
+# @Last Modified by:   cnicolas
+# @Last Modified time: 2015-11-25 16:50:56
 
 import logging
 
 from battle.models import AttackByClasse, PlayerArmor, Player
+from battle.utils import armorWeightByName
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +16,8 @@ class ArmorPieceDto:
 	def __init__(self, armor):
 		self.id = armor.id
 		self.name = armor.name
+		self.place = armor.place
+		self.weight = armor.weight
 		self.price = armor.price
 		self.defense = armor.defense
 		self.health = armor.health
@@ -23,13 +26,13 @@ class ArmorPieceDto:
 		self.intellect = armor.intellect
 		self.agility = armor.agility
 		self.spirit = armor.spirit
-		self.weight = armor.category.weight
-		self.place = armor.category.place
 
 	def toDictionnary(self):
 		res = {}
 		res['id'] = self.id
 		res['name'] = self.name
+		res['place'] = self.place
+		res['weight'] = self.weight
 		res['price'] = self.price
 		res['defense'] = self.defense
 		res['health'] = self.health
@@ -38,9 +41,6 @@ class ArmorPieceDto:
 		res['agility'] = self.agility
 		res['intellect'] = self.intellect
 		res['spirit'] = self.spirit
-		res['weight'] = self.weight
-		res['place'] = self.place
-
 		return res
 
 class PlayerDto:
@@ -57,7 +57,7 @@ class PlayerDto:
 		self.spirit = player.spirit
 		self.classe = player.classe
 
-		self.amor_type = self.classe.armor_type
+		self.weight = armorWeightByName[self.classe.weight]
 
 		abc = AttackByClasse.objects.filter(classe=self.classe)
 		pa = PlayerArmor.objects.filter(player=player)
@@ -65,12 +65,12 @@ class PlayerDto:
 		self.attacks = [a.attack for a in abc]
 		self.armors = [ArmorPieceDto(a.armor) for a in pa]
 
-		self.armor_head = self.find_armor_by_place("Head")
-		self.armor_torso = self.find_armor_by_place("Torso")
-		self.armor_hand = self.find_armor_by_place("Hand")
-		self.armor_neck = self.find_armor_by_place("Neck")
-		self.armor_feet = self.find_armor_by_place("Feet")
-		self.weapon = self.find_armor_by_place("Weapon")
+		self.armor_head = self.find_armor_by_place("head")
+		self.armor_torso = self.find_armor_by_place("torso")
+		self.armor_hand = self.find_armor_by_place("hand")
+		self.armor_neck = self.find_armor_by_place("neck")
+		self.armor_feet = self.find_armor_by_place("feet")
+		self.weapon = self.find_armor_by_place("weapon")
 
 	def find_armor_by_place(self, place):
 		for a in self.armors:
--- battle/views.py
+++ battle/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 from battle.models import Classe, Attack, AttackByClasse, ArmorPiece, Player, PlayerArmor
@@ -20,7 +21,19 @@ class IndexView(TemplateView):
 #####################################################################################################
 
 def test(request):
-	return myRender(request, 'test.html', context)
+	classe = Classe.objects.filter(name='Guerrier')[0]
+	aku = Player.objects.create_player("Aku_Guerrier", classe, 20, 0, 200, 100, 100, 20, 10, 10, 10)
+	head = ArmorPiece.objects.filter(place='head', weight='heavy')[0]
+	torso = ArmorPiece.objects.filter(place='torso', weight='heavy')[0]
+	hand = ArmorPiece.objects.filter(place='hand', weight='heavy')[0]
+	neck = ArmorPiece.objects.filter(place='neck', weight='heavy')[0]
+	feet = ArmorPiece.objects.filter(place='feet', weight='heavy')[0]
+	PlayerArmor.objects.create_player_armor(aku, head)
+	PlayerArmor.objects.create_player_armor(aku, torso)
+	PlayerArmor.objects.create_player_armor(aku, hand)
+	PlayerArmor.objects.create_player_armor(aku, neck)
+	PlayerArmor.objects.create_player_armor(aku, feet)
+	return redirect('show')
 
 def show(request):
 	# return HttpResponse(Classe.objects.all())
